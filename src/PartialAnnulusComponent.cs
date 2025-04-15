bd5df1c6431b375dda229aaedef80f0131cb247c
David Rutten <david@mcneel.com>
2025-04-15 12:46:46 +0100

    Type assistant added.

--- src/PartialAnnulusComponent.cs
+++ src/PartialAnnulusComponent.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Collections.Generic;
+using Eto.Forms;
 using Grasshopper2;
 using Grasshopper2.Components;
+using Grasshopper2.Data;
+using Grasshopper2.Display;
 using Grasshopper2.Extensions;
+using Grasshopper2.Types.Assistant;
+using Grasshopper2.Types.Fields;
 using Grasshopper2.Types.Numeric;
 using Grasshopper2.UI;
 using GrasshopperIO;
@@ -192,6 +198,31 @@ namespace S2FDemo
     #endregion
   }
 
+  public sealed class PartialAnnulusTypeAssistant : TypeAssistant<PartialAnnulus>
+  {
+    public PartialAnnulusTypeAssistant() : base("Partial Annulus") { }
+
+    public override PartialAnnulus Copy(PartialAnnulus instance)
+    {
+      return instance;
+    }
+
+    public override string DescribePrimary(Pear<PartialAnnulus> pear)
+    {
+      return pear.Item.ToString();
+    }
+
+    public override void Draw(Pear<PartialAnnulus> pear, DisplayBag bag)
+    {
+      var dc = new DisplayCurve(pear.Item.ToRhinoCurve(), CurveKind.Curve, pear.Meta);
+      bag.AddCurve(dc);
+    }
+    public override IEnumerable<GeometryBase> ToGeometry(Pear<PartialAnnulus> pear)
+    {
+      yield return pear.Item.ToRhinoCurve();
+    }
+  }
+
   [IoId("06141644-93bd-4518-8006-4580033e0727")]
   public sealed class AnnulusComponent : Component
   {
@@ -200,16 +231,34 @@ namespace S2FDemo
 
     protected override void AddInputs(InputAdder inputs)
     {
-
+      inputs.AddPlane("Plane", "Pl", "Annulus base plane.").Set(Plane.WorldXY);
+      inputs.AddField("Inner Radius", "Ri", "Annulus inner radius.").Set(1);
+      inputs.AddField("Outer Radius", "Ro", "Annulus outer radius.").Set(1.5);
+      inputs.AddField("Sweep Factor", "Sf", "Annulus sweep factor.").Set(0.75);
     }
     protected override void AddOutputs(OutputAdder outputs)
     {
-
+      outputs.AddGeneric("Annulus curve", "Ac", "Result");
     }
 
     protected override void Process(IDataAccess access)
     {
+      access.GetItem(0, out Plane plane);
+      access.GetItem(1, out Field inner);
+      access.GetItem(2, out Field outer);
+      access.GetItem(3, out Field sweep);
 
+      var ri = inner.ScalarAt(plane.Origin);
+      var ro = outer.ScalarAt(plane.Origin);
+      var sw = sweep.ScalarAt(plane.Origin);
+
+      access.RectifyPositive(ref ri, "inner radius");
+      access.RectifyPositive(ref ro, "outer radius");
+      access.RectifyDomain(ref sw, (0, 1), "sweep factor");
+
+      var annulus = new PartialAnnulus(plane, ri, ro, sw);
+
+      access.SetItem(0, annulus);
     }
   }
 }
